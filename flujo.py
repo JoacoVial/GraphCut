f600fc73d7b0fa81eef202dd43fce899ec4d7e49
JoacoVial <64756805+JoacoVial@users.noreply.github.com>
2021-12-11 12:14:18 -0300

    asd

--- flujo.py
+++ flujo.py
@@ -97,7 +97,7 @@ KBKG = K.puntos("SELECCIONE LOS PUNTOS DEL FONDO")
 TLINKS_OBJ = tlinks(obj, KOBJ, KBKG, peso)
 TLINKS_BGK = tlinks(bkg, KBKG, KOBJ, peso)
 
-g.add_grid_tedges(nodeids, KOBJ, KBKG)
+g.add_grid_tedges(nodeids, TLINKS_OBJ, TLINKS_BKG)
 
 flow = g.maxflow()
 print(f"\n\nFlujo máximo: {flow}\n")
